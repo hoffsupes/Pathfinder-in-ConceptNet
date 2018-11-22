e175369141abe14cf955e089c128f351bd7c30e3
hoffsupes <dassgrv@gmail.com>
2018-11-22 08:14:11 -0500

    Add files via upload

---
+++ relation.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import json
+import pdb
+import sys
+
+def isa_query_creator(node,mode):
+	if(mode):
+		query = 'http://api.conceptnet.io/query?node=/c/en/'+ node +'&rel=/r/IsA'
+	else:
+		query = 'http://api.conceptnet.io/query?node='+ node +'&rel=/r/IsA'
+	return query;
+
+def rel_query_creator(start,end,mode):
+	if(mode):
+		query = 'http://api.conceptnet.io/relatedness?node1=/c/en/'+ start + '&node2=/c/en/' + end;
+	else:
+		query = 'http://api.conceptnet.io/relatedness?node1='+ start + '&node2=' + end;		
+	return query;
+
+def get_next_node(start,end):
+	rel_list = [];
+	ter_list = [];
+	t_list = []; # for possible future use
+	obj = requests.get(isa_query_creator(start,1)).json();
+
+	for i in range(0,len(obj['edges'])):
+		if 'IsA' in obj['edges'][i]['rel']['label']:
+			ter_list.append(obj['edges'][i]['end']['term']);
+			t_list.append(obj['edges'][i]['end']['term'][6:len(obj['edges'][i]['end']['term'])]);
+			rel_list.append(requests.get(rel_query_creator(obj['edges'][i]['end']['term'][6:len(obj['edges'][i]['end']['term'])],end,1)).json()['value']);
+	
+	t_new = [i for i,m in enumerate(t_list) if m == start ];		## delete any self references
+	t_new.sort(reverse=True);
+	for i in t_new:
+		del(t_list[i]);
+		del(ter_list[i]);
+		del(rel_list[i]);
+	
+	if(rel_list == []):
+		return 'null','null','FX';
+	
+	newi = rel_list.index(max(rel_list));
+	
+	if([i for i in t_list if end in i] != []):
+		return end,'/c/en/'+end,'EX';
+	
+	return t_list[newi],ter_list[newi],rel_list[newi];
+
+def get_full_list(start,end):
+	trav_list = [];
+	oldstart = start;
+	trav_list.append(start);
+	mode = 0;
+	while(1):
+		name,fullname,relness = get_next_node(start,end);
+		if (type(relness) == type('string value')):
+			if(relness == 'FX'):
+				print('There was no link to the final node for ',oldstart);
+				mode = 1;
+				break;
+		if(name.lower() == trav_list[len(trav_list) - 1].lower()):
+			print('There was no link to the final node for ',oldstart);
+			mode = 1;
+			break;
+		if(name.lower() == end.lower()):
+			trav_list.append(end);
+			break;
+		trav_list.append(name);
+		start = name;
+	return trav_list,mode;
+h = sys.argv;
+li,mo = get_full_list(h[1],h[2]);
+print(li);
+# requests.get('http://api.conceptnet.io/relatedness?node1=/c/en/tea&node2=/c/en/one_variety_of_tea').json(); # related nodes
