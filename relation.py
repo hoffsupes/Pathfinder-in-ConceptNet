1e462d27bb26f60f3c1bd2fd3085add6818e3159
hoffsupes <dassgrv@gmail.com>
2018-11-26 13:26:52 -0500

    Update relation.py
    Introducing two modes:
        Approx: A lax search for relationships
        Full: A stringent search for relationships

--- relation.py
+++ relation.py
@@ -4,6 +4,21 @@ import json
 import pdb
 import sys
 
+### Usage python relation.py item1 item2 searchtype
+### searchtype = {approx,full}
+### approx = if any simple isA match is found, even in terms of the labels of the categories, it is a match
+### full = a stricter search
+### Edge case, when trying something that is not food
+### python relation.py bowl food approx
+### Result: 
+### ['bowl','food']
+### But bowl is not a kind of food!
+### Let's try again
+### python relation.py bowl food full
+### Result:
+### There was no link to the final node for  bowl
+### ['bowl', 'food_vessel', 'kitchenware', 'housewares', 'goods', 'generic_artifact', 'cultural_thing']
+
 def isa_query_creator(node,mode):
 	if(mode):
 		query = 'http://api.conceptnet.io/query?node=/c/en/'+ node +'&rel=/r/IsA'
@@ -18,7 +33,7 @@ def rel_query_creator(start,end,mode):
 		query = 'http://api.conceptnet.io/relatedness?node1='+ start + '&node2=' + end;		
 	return query;
 
-def get_next_node(start,end):
+def get_next_node(start,end,mode):
 	rel_list = [];
 	ter_list = [];
 	t_list = []; # for possible future use
@@ -41,10 +56,10 @@ def get_next_node(start,end):
 		return 'null','null','FX';
 	
 	newi = rel_list.index(max(rel_list));
-	
-	if([i for i in t_list if end in i] != []):
-		return end,'/c/en/'+end,'EX';
-	
+	if mode == 'approx':
+		if([i for i in t_list if end in i] != []):
+			return end,'/c/en/'+end,'EX';
+	#print(t_list[newi])
 	return t_list[newi],ter_list[newi],rel_list[newi];
 
 def get_full_list(start,end):
@@ -53,7 +68,7 @@ def get_full_list(start,end):
 	trav_list.append(start);
 	mode = 0;
 	while(1):
-		name,fullname,relness = get_next_node(start,end);
+		name,fullname,relness = get_next_node(start,end,h[3]);
 		if (type(relness) == type('string value')):
 			if(relness == 'FX'):
 				print('There was no link to the final node for ',oldstart);
@@ -73,3 +88,4 @@ h = sys.argv;
 li,mo = get_full_list(h[1],h[2]);
 print(li);
 # requests.get('http://api.conceptnet.io/relatedness?node1=/c/en/tea&node2=/c/en/one_variety_of_tea').json(); # related nodes
+
